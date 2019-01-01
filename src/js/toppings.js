cd96d8f5ab189aeb05897d8b6326bf1cb3896591
scott <scottcurrell@gmail.com>
2018-12-31 22:37:29 -0700

    toppings working

--- src/js/toppings.js
+++ src/js/toppings.js
@@ -18,30 +18,9 @@ YB.toppings = {
 // YB.toppings.classLists.add();
 // YB.toppings.style.background = 'url(/assets/img/M&M.jpg)';
 
-// [].forEach.call(YB.toppings.el, function(el) {
-//     console.log(el);
-// });
-
-// for (el of YB.toppings.el) {
-//     console.log(el);
-// }
-
 for (var i = 0; i < YB.toppings.el.length; i++) {
-    YB.toppings.el[i].classList.add('derp');
+    var _el = YB.toppings.el[i];
+    var _bgImg = _el.getAttribute('data-img');
+
+    _el.style.background = 'url(' + _bgImg + ')';
 }
-
-(function() {
-    console.log('Hello, world!');
-})();
-
-console.log('derp');
-
-// var materials = [
-//   'Hydrogen',
-//   'Helium',
-//   'Lithium',
-//   'Beryllium'
-// ];
-
-// console.log(materials.map(material => material.length));
-// expected output: Array [8, 6, 7, 9]
